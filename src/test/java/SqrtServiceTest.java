aa442ceadf1a56131c64dfa4c4b64cd9cc4bfa2c
Maximus301191 <maxalut301191@gmail.com>
2025-02-02 19:51:07 +0300

    Parameterized Test

--- src/test/java/SqrtServiceTest.java
+++ src/test/java/SqrtServiceTest.java
@@ -1,17 +1,20 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa.javaqamvn1.services.SqrtService;
 
 public class SqrtServiceTest {
 
-    @Test
-    public void shouldCalcExact() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/java/resources/data.csv")
+        public void shouldCalcExact(int income, int expenses, int treshold, int expected) {
         SqrtService service = new SqrtService();
 
-        int income = 100_000;
-        int expenses = 60_000;
-        int treshold = 150_000;
-        int expected = 2;
+        //int income = 100_000;
+        //int expenses = 60_000;
+        //int treshold = 150_000;
+        //int expected = 2;
         int actual = service.calcsqrt(income, expenses, treshold);
         System.out.println(actual + " месяца(ев) в году отдыхал работник");
         Assertions.assertEquals(expected, actual);
